412c23b485d8580b32eaecb11d3ae11c6401c9c8
ExtremeBlackLiu <54531236+extremeblackliu@users.noreply.github.com>
2022-02-03 20:19:02 +0800

    Add files via upload

---
+++ bootstrap/bootstrap.cpp
@@ -0,0 +1,47 @@
+#include "bootstrap.h"
+#include "../minhook/include/MinHook.h"
+
+namespace Hooks
+{
+
+	void Init(HMODULE mod)
+	{
+		while (!GetModuleHandleA("serverbrowser.dll"))
+		{
+			Sleep(2000);
+		}
+		//55 8B EC 56 8B F1 33 C0 57 8B 7D 08 8B 8E ? ? ? ? 85 C9 7E 12 3B 3C 86 - client.dll + engine.dll + studiorender.dll
+		HMODULE client, engine, studiorender, materialsystem;
+		client = GetModuleHandleA("client.dll");
+		engine = GetModuleHandleA("engine.dll");
+		studiorender = GetModuleHandleA("studiorender.dll");
+		materialsystem = GetModuleHandleA("materialsystem.dll");
+		
+		const char* sig = "55 8B EC 56 8B F1 33 C0 57 8B 7D 08 8B 8E ? ? ? ? 85 C9 7E 12 3B 3C 86";
+
+		void* result = Utils::PatternScan(client, sig);
+		void* result1 = Utils::PatternScan(engine, sig);
+		void* result2 = Utils::PatternScan(studiorender, sig);
+		void* result3 = Utils::PatternScan(materialsystem, sig);
+		if (!result || !result1 || !result2 || !result3)
+		{
+			MessageBoxA(0, "ÈÆ¹ýÊ§°Ü", "LauncherSU.net", 0);
+			FreeLibraryAndExitThread(mod, 0);
+			return;
+		}
+		MH_Initialize();
+		void* orig = nullptr;
+		MH_CreateHook(result, &hk_VerifyReturnAddress, &orig);
+		MH_CreateHook(result1, &hk_VerifyReturnAddress, &orig);
+		MH_CreateHook(result2, &hk_VerifyReturnAddress, &orig);
+		MH_CreateHook(result3, &hk_VerifyReturnAddress, &orig);
+		MH_EnableHook(MH_ALL_HOOKS);
+	}
+
+	bool __fastcall hk_VerifyReturnAddress(void* ecx, void* rdx,const char* modulename)
+	{
+		return true;
+	}
+
+
+}
---
+++ dllmain.cpp
@@ -0,0 +1,33 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include <Windows.h>
+
+HMODULE g_module = nullptr;
+
+#include "bootstrap/bootstrap.h"
+
+
+void Thread()
+{
+    Hooks::Init(g_module);
+    
+    return;
+
+}
+
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        DisableThreadLibraryCalls(hModule);
+        g_module = hModule;
+        CreateThread(0, 0, (LPTHREAD_START_ROUTINE)Thread, 0, 0, 0);
+        break;
+    }
+    return TRUE;
+}
+
